f50d7e0c339cd2f91a9a6cc1603706a46cd94d52
rosenset <61257952+rosenset@users.noreply.github.com>
2020-07-08 19:21:56 -0400

    second commit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,3 +1,78 @@
+// import { Component, OnInit } from '@angular/core';
+
+// interface List {
+//   task:string;
+//   completed:boolean;
+//   editing:boolean;
+// }
+
+
+// @Component({
+//   selector: 'app-todo',
+//   templateUrl: './todo.component.html',
+//   styleUrls: ['./todo.component.css']
+// })
+// export class TodoComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+
+//   title = 'TODO List';
+//   addInput:string;
+  
+//   filterText:string;
+//   completed:boolean=true;
+  
+//   items: List []=[
+//     {task: 'Walk the dog.',completed:false,editing:false},
+//     {task: 'Go to the store.',completed:true,editing:false},
+//     {task: 'Walk the cat',completed:false,editing:false},
+//     {task: 'Walk the car',completed:false,editing:false},
+//   ]
+//   filteredTodo=[...this.items];
+//   addTask =() => {
+//     const newTask = {
+//       task:this.addInput,
+//       completed:false,
+//       editing:false
+//     }
+//     this.items.push(newTask);
+//     this.filteredTodo= [...this.items];
+//     this.addInput=null;
+//   }
+  
+
+//   completeTask=(i)=>{
+//     this.items[i].completed=true;
+    
+//   }
+
+
+//   removeTask = (i) => {
+//     this.items.splice(i, 1);
+//     this.filteredTodo= [...this.items];
+//   }
+  
+  
+//   filterTodos = ()=> {
+//     console.log(this.filteredTodo);
+//     this.filteredTodo=this.items.filter(item=>item.task.includes(this.filterText));
+    
+//   }
+
+// editTodo(item:List):void {
+//   item.editing = true;
+// }
+
+// doneEdit(item:List):void {
+//   item.editing=false;
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
 
 interface List {
@@ -28,9 +103,9 @@ export class TodoComponent implements OnInit {
   
   items: List []=[
     {task: 'Walk the dog.',completed:false,editing:false},
-    {task: 'Go to the store.',completed:true,editing:false},
-    {task: 'Walk the cat',completed:false,editing:false},
-    {task: 'Walk the car',completed:false,editing:false},
+    {task: ' walk the line.',completed:true,editing:false},
+    {task: 'Walk it out',completed:false,editing:false},
+    {task: 'Walk hard',completed:false,editing:false},
   ]
   filteredTodo=[...this.items];
   addTask =() => {
